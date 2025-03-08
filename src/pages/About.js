f9ed1f404ac3a2637ad7d906bdeca2ce480c7145
seankql <verylausean@gmail.com>
2025-03-07 19:11:46 -0500

    Initialized portfolio

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import "../styles/About.css";
+
+export default function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>I am a software engineer passionate about building cool stuff.</p>
+    </section>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import "../styles/Home.css";
+
+export default function Home() {
+  return (
+    <section className="home">
+      <h2>Welcome to My Portfolio</h2>
+      <p>This is a simple portfolio built with React.</p>
+    </section>
+  );
+}
