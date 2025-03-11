1f674ac0b30ca4d6f48d2e56e247543d98166469
seankql <verylausean@gmail.com>
2025-03-11 13:53:35 -0400

    Initialized website

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import React from "react";
+import { BrowserRouter as Router} from "react-router-dom";
 import "./styles/App.css";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import "../styles/Footer.css";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>&copy; 2025 My Portfolio. All rights reserved.</p>
-    </footer>
-  );
-}
