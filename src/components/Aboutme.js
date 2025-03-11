aae4a387c8c4117723d270e20642906952f0f87a
seankql <verylausean@gmail.com>
2025-03-11 18:46:49 -0400

    Updated about me with icons and links

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/AboutMe.css";
-import { ReactComponent as Icon1 } from "../assets/icon1.svg";
-import { ReactComponent as Icon2 } from "../assets/icon2.svg";
-import { ReactComponent as Icon3 } from "../assets/icon3.svg";
+import { ReactComponent as EmailIcon } from "../assets/email-icon.svg";
+import { ReactComponent as LinkedinIcon } from "../assets/linkedin-icon.svg";
+import { ReactComponent as GithubIcon } from "../assets/github-icon.svg";
+import { ReactComponent as LocationIcon } from "../assets/location-icon.svg";
+import { ReactComponent as TitleIcon } from "../assets/job-icon.svg";
 
 function AboutMe() {
-    const [copied, setCopied] = useState(false);
-
     const handleCopyEmail = () => {
         const email = "verylausean@gmail.com";
-        navigator.clipboard.writeText(email)
-            .then(() => {
-                setCopied(true);
-                setTimeout(() => setCopied(false), 2000);
-            })
-            .catch((err) => {
-                console.error("Failed to copy email: ", err);
-            });
+        navigator.clipboard.writeText(email).catch((err) => {
+            console.error("Failed to copy email: ", err);
+        });
     };
 
     return (
@@ -24,18 +19,25 @@ function AboutMe() {
             <div className="about-me-box">
                 <div>
                     <h1>Sean Lau</h1>
-                    <h4>Software Engineer @ Toronto, CA</h4>
+                    <div className="title-box">
+                        <TitleIcon className="icon" />
+                        <span className="title-text">Software Engineer</span>
+                    </div>
+                    <div className="location-box">
+                        <LocationIcon className="icon" />
+                        <span className="location-text">Toronto, CA</span>
+                    </div>
+                    <div className="email-box">
+                        <EmailIcon className="icon" onClick={handleCopyEmail} />
+                        <span className="email-text">verylausean@gmail.com</span>
+                    </div>
                 </div>
                 <div>
-                    <div className="email-box">
-                        <Icon1 className="icon" onClick={handleCopyEmail} />
-                        {copied && <span className="copy-notification">Email Copied!</span>}
-                    </div>
                     <a href="https://www.linkedin.com/in/sean-lau-445239226/" target="_blank" rel="noopener noreferrer">
-                        <Icon2 className="icon" />
+                        <LinkedinIcon className="icon" />
                     </a>
                     <a href="https://github.com/seankql" target="_blank" rel="noopener noreferrer">
-                        <Icon3 className="icon" />
+                        <GithubIcon className="icon" />
                     </a>
                 </div>
             </div>
