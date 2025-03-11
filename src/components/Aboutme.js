fd34496e218e9f89f40c242e79d0c2b4f4458aed
seankql <verylausean@gmail.com>
2025-03-11 14:11:59 -0400

    Added smooth scrolling

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -3,6 +3,7 @@ const AboutMe = () => {
       <section id="about-me">
         <h2>About Me</h2>
         <p>Introduce yourself here—background, skills, passions.</p>
+        <p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p><p>Introduce yourself here—background, skills, passions.</p>
       </section>
     );
   };
